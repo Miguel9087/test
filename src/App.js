357dc380221874ff57c4378fa204d745f880610d
TonyM <antonio@alchimia.mx>
2018-08-23 13:53:25 -0500

    fix: Add a new line of code

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ class App extends Component {
           <p>hecho por Miguel par hacer pruebas del ejercicio</p>
           <p>Lo que sea: Hecho por TonyM </p>
           <p>Lo que sea: Hecho por TonyM doble vez </p>
+          <p>Lo que sea: Hecho por TonyM triple vez </p>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
