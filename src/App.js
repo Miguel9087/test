ebefd7a2ebe9ea23d11b9cd0c7e1ab29e79cc764
miguel <miguel@alchimia.mx>
2018-08-23 15:06:05 -0500

    fix: Add two lines of code

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ class App extends Component {
           <p>hecho por Miguel par hacer pruebas del ejercicio</p>
           <p>hecho por Miguel par hacer pruebas del ejercicio por segunda vez</p>
           <p>hecho por Miguel par hacer pruebas del ejercicio por segunda vez</p>
+          <p>hecho por Miguel par hacer pruebas del ejercicio por segunda vez</p>
           <p>Lo que sea: Hecho por TonyM </p>
           <p>Lo que sea: Hecho por TonyM doble vez </p>
           <p>Lo que sea: Hecho por TonyM triple vez </p>
